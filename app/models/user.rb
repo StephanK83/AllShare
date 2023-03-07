5c9f360b50ffe0e428071f853974021b754e9cfc
Lorenzo <lorenzo.bertolino@gmail.com>
2023-03-07 21:17:55 +0100

    done user associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :bookings # bookings as a client
+  has_many :bookings_as_owner, through: :items, source: :bookings
+  validates :email, :first_name, :last_name, :street_name, :city, :postal_code, :country, :phone, :password,  presence: true
 end
