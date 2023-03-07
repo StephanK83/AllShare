558488942d1959a7099ca0b2b7ced4da6a503602
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-03-07 14:20:57 +0200

    nested routes to be validated by TA

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
+  resources :items do
+    resources :bookings do
+      resources :reviews
+    end
+    resources :favourites
+  end
 end
