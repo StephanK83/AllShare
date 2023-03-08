7374a7e41c8940df678919bd30886e328f3621cd
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-03-08 19:14:44 +0200

    chatroom in progress 1h10 tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_195149) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_145437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_195149) do
     t.date "end_date"
     t.float "total_price"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -34,6 +39,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_195149) do
     t.integer "quantity"
     t.integer "min_days_rent"
     t.string "postal_code"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_195149) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
