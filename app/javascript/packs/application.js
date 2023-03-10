c92bf47ceb2337a1ec77dfa75a91d13b9c2a916d
Stephan Knorn <stephan.knorn@berlin101.de>
2023-03-10 10:52:18 +0100

    played with views. Not all changed files should be merged. There is some useless stuff

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,8 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+// app/javascript/packs/application.js
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faHeart)
