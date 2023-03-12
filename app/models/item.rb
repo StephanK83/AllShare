630741a7d54b8a5c8198af05f773d1030f1b323c
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-03-12 20:14:35 +0200

    search feature done

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   validates :category, :name, :description, :postal_code, :quantity, :min_days_rent, :price, presence: true
 
   acts_as_favoritable
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_and_category,
+    against: [ :name, :category ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
