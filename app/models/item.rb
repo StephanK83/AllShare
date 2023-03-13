5312f34228659d631ccceceba9d0d8468745da67
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-03-13 16:15:37 +0200

    cloudinary in progress

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many :reviews, through: :bookings
   belongs_to :user
 
+
   validates :category, :name, :description, :postal_code, :quantity, :min_days_rent, :price, presence: true
 
   acts_as_favoritable
