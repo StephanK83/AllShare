a0a02f0e0941812e3ea69f9b1e58c9c687fbb160
Lorenzo <lorenzo.bertolino@gmail.com>
2023-03-15 21:27:28 +0100

    created >20 items seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_11_125714) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_174932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_11_125714) do
     t.integer "quantity"
     t.integer "min_days_rent"
     t.string "postal_code"
+    t.string "picture"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
