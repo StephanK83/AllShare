e2d89b8637bca168f6339668a4abc90f2e51b702
Lorenzo <lorenzo.bertolino@gmail.com>
2023-03-15 18:40:58 +0100

    Seeds work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "yaml"
+require "httparty"
+
+response = HTTParty.get('https://jsonplaceholder.typicode.com/users')
+users = JSON.parse(response.body)
+
+users.each do |user|
+  User.create!(
+    email: user['email'],
+    first_name: user['name'].split(' ').first,
+    last_name: user['name'].split(' ').last,
+    street_name: user['address']['street'],
+    postal_code: user['address']['zipcode'],
+    city: user['address']['city'],
+    password: "123456"
+  )
+end
+
+file_items = File.open("/home/lorenzo/code/lorenzb58/AllShare/storage/Items_seeds.json")
+items = JSON.parse(file_items.read)
+
+
+p items
