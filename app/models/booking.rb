15baf7bd6ce33e3c7570e07cb35c945ed60680db
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-03-16 14:04:27 +0200

    calendar using flatpickr

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :item
 
   validates :start_date, :end_date, presence: true
+  validate :min_days_rent
   enum status: { pending: 0, accepted: 1, declined: 2, cancelled: 3 }
 
   def total_price
@@ -11,4 +12,9 @@ class Booking < ApplicationRecord
     return (item.price * number_of_days).to_i
   end
 
+  def min_days_rent
+    if start_date && end_date && (end_date - start_date).to_i < item.min_days_rent
+      errors.add(:end_date, "must be at least #{item.min_days_rent} days after the start date")
+    end
+  end
 end
