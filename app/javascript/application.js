0fd97ec683258d5357d2a03b97e03683d112a53a
Stephan Knorn <Stephan.knorn@berlin101.de>
2023-03-26 19:12:51 +0200

    adapted login to signup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,13 @@
 // Entry point for the build script in your package.json
 //= require fontawesome
 //= require jquery
+//= require_tree
+//= require jquery_ujs
+//= require bootstrap
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 import "jquery-ujs"
+
+//= require overlay
---
+++ app/javascript/overlay.js
@@ -0,0 +1,9 @@
+function showLogin() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("login-form-wrapper").style.display = "block";
+}
+
+function hideLogin() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("login-form-wrapper").style.display = "none";
+}
